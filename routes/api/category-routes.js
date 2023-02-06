f65d01ec7ad2980131d40d83f831dcc54eb59ade
Danielle Cavinder <daniellecavinder@gmail.com>
2023-02-05 20:23:05 -0700

    added routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,71 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // finds all of the categories
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // finds a single category by id
+  try {
+   const categoryData = await Category.findByPk(req.params.id, {
+    include: [{ model: Product }]
+  })
+  res.status(200).json(categoryData)
+} catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// creates a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategoryData = await Category.create(req.body);
+    res.status(200).json(newCategoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+//updates the category by its id
+router.put('/:id', async (req, res) => {
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(updateCategory)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+//deletes a category by its id
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteCategoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(deleteCategoryData)
+
+    if (!delCatData) {
+      res.status(404).json({message: 'This category does not exist'})
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
